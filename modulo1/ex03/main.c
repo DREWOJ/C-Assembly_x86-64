01e190bf1b781ab2eaf16cb38cf901e5d04d6248
AndreBarros1 <1211299@isep.ipp.pt>
2022-10-02 16:59:50 +0100

    fix: ex03 mod1 tests OK

--- modulo1/ex03/main.c
+++ modulo1/ex03/main.c
@@ -1,7 +1,7 @@
 #include "sum_even.h"
 
 int main() {
-	int vec[] = {1,2,2,6};
+	int vec[] = {1,1,0,0};
 	int num = 4;
 	sum_even(vec,num);
 	return 0;
--- modulo1/ex03/sum_even.c
+++ modulo1/ex03/sum_even.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
 int sum_even(int *p, int num) {
 	int sum=0;
+	
 	for (int i = 0; i<num; i++) {
 			if (p[i] % 2 == 0) {
 				sum = sum + p[i];
 			}
 	}
-	printf("sum = %d\n", sum);	
-	return 0;
+	
+	return sum;
 }
