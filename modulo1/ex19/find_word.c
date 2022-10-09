9a71f1663a7413632fa5e74aae995c89c399b641
carlos <1211277@isep.ipp.pt>
2022-10-09 22:23:38 +0100

    shor: added comments

--- modulo1/ex19/find_word.c
+++ modulo1/ex19/find_word.c
@@ -1,5 +1,7 @@
 #include "stddef.h"
-int not_equal(char a, char b){
+
+//compares char regardless of lower/capital cases
+int not_equal(char a, char b){ 
 	// checks if it is a lower case letter
 	if(a >= 97 && a <= 122) a -= 32;
 	if(b >= 97 && b <= 122) b -= 32;
@@ -7,17 +9,22 @@ int not_equal(char a, char b){
 	return 1;
 }
 
+//similar function to ex 16
 char* find_word(char* word, char* initial_addr){
 	char *result = NULL;
 	int i = 0;
 
+	//while the string isnt over and result is null (result null means word wasn't found yet)
 	while(*initial_addr != '\0' && result == NULL){ 
+
+		//if the char matches the first char of the word
 		if(!not_equal(*word, *initial_addr)){
 			result = initial_addr;
 			i = 1;
-
+			//while word ins't over and result isnt null (result null means it is a possible word to find)
 			while(*(word + i) != '\0' && result != NULL){
 				
+				// if the char doesnt match result turns to null which means word wasn't found
 				if(not_equal(*(word + i), *(result + i))){
 					result = NULL;
 				}
