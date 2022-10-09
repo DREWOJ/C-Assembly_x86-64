2f21ea14b88d920e989bda9511d276466e260a53
André Barros <andrebarros0426@gmail.com>
2022-10-09 18:58:13 +0100

    fix: ex10 mod1 tests OK

--- modulo1/ex10/odd_sum.c
+++ modulo1/ex10/odd_sum.c
@@ -1,7 +1,7 @@
 int odd_sum(int *p){
 	int sum = 0;
-	for(int i = 1; i < *p; i++){
-		if(*(p+i)%2 != 0) sum += *(p+i);
+	for(int i = 1; i <= *p; i++){ // for each element of the array checks if it is an odd number
+		if(*(p+i) % 2 != 0) sum += *(p+i);
 	}
 	return sum;
 }
