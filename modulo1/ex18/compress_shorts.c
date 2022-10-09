f22d6dafdb378d48fc8c770dd832758acf5c3914
carlos <1211277@isep.ipp.pt>
2022-10-09 20:13:48 +0100

    fix: ex18 mod1

--- modulo1/ex18/compress_shorts.c
+++ modulo1/ex18/compress_shorts.c
@@ -2,6 +2,7 @@
 
 void compress_shorts(short* shorts, int n_shorts, int* integers){
 	for(int i = 0; i<n_shorts; i+=2){
-		integers[i/2] = (shorts[i] << 16) + shorts[i+1];
+		int temp = shorts[i];
+		integers[i/2] = (temp << 16) + shorts[i+1];
 	}
 }
