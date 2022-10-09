1365f41ab0a5156a68422e60aef5610d662a1ed8
André Barros <andrebarros0426@gmail.com>
2022-10-09 18:52:08 +0100

    fix: ex12mod1 tests OK

--- modulo1/ex12/capitalize.c
+++ modulo1/ex12/capitalize.c
@@ -1,8 +1,13 @@
 void capitalize (char *str){
+	
+	if(*str >= 97 && *str <=122) { // check the first letter of the string
+		*str -=32;
+	}
+	
 	while(*str != 0){
 		if(*str == ' '){
-			if(*(str+1) >= 97 && *(str+1) <= 122){ // checks lower case letter
-				*(str+1) -= 32; // capitalizes
+			if(*(str + 1) >= 97 && *(str + 1) <= 122){ // checks lower case letter
+				*(str + 1) -= 32; // capitalizes
 				str++;
 			}
 		}
