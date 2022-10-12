8fe15d8939932b2558785fa63432a3d2efd7652f
André Barros <andrebarros0426@gmail.com>
2022-10-12 17:15:33 +0100

    feat: ex03 mod2

---
+++ modulo2/ex03/sum_v2.s
@@ -0,0 +1,20 @@
+.section .data
+	.global op1
+	.global op2
+	.equ CONST, 20
+
+.section .text
+	.global sum_v2 # int sum_v2(void)
+
+sum_v2:
+
+	movl op1(%rip), %ecx # place op1 in ecx
+	movl op2(%rip), %eax # place op2 in eax
+	movl $CONST ,%edx # place CONST in edx
+	
+	addl %ecx, %ecx # add ecx to ecx
+	subl %ecx, %edx  # sub const to ecx
+
+	addl %edx, %eax # add ecx to eax
+	
+ret 
