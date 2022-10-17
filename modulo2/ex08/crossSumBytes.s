f33533f8c7c789939898d2a842f3c08fecc19c7a
carlos <1211277@isep.ipp.pt>
2022-10-17 16:20:21 +0100

    feat: ex08 mod2

---
+++ modulo2/ex08/crossSumBytes.s
@@ -0,0 +1,22 @@
+.section .data
+	.global s1
+	.global s2
+	
+	s1:
+		.short 0 
+	
+	s2:
+		.short 0 
+	
+.section .text
+	.global crossSumBytes # short crossSumBytes(void)
+	
+crossSumBytes:
+	movw s1(%rip), %cx # place s1 into cx
+	movw s2(%rip), %dx # place s2 into dx
+	
+	addb %dl, %ch # add byte dl of dx into ch of cx
+	addb %dh, %cl # add byte dh of dx into cl of cx
+	
+	movw %cx, %ax
+ret
---
+++ modulo2/ex08/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "asm.h"
+
+extern short s1;
+extern short s2;
+
+int main() {
+	
+short n = crossSumBytes();
+
+return 0;	
+}
