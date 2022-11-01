eaefc62e979d21da5f3fb6a0dd425fe9442ea2c0
carlos <1211277@isep.ipp.pt>
2022-11-01 22:00:24 +0000

    fix: ex14 mod3

--- modulo3/ex14/exists.s
+++ modulo3/ex14/exists.s
@@ -7,68 +7,61 @@
 	.global exists # int exists(void)
 	.global vec_diff # int vec_diff(void)
 	
-vec_diff:
-	movl $0, x(%rip)
-
-	movl $0, %edi # set edi (result) to 0
-	movq num(%rip), %rcx # mov vec lenght to rcx
-	movq ptrvec(%rip), %r9 # move pointer to r9
-
-	cmpl $0, num(%rip)
-	je end_vec_diff
-
-loop_vec_diff:
-	
-	movl (%r9), %eax
-	movl %eax, x(%rip) # move pointed value to x
-
-	pushq %rcx
-
-	call exists
-
-	popq %rcx
-
-	addl %eax, %esi
-
-	addq $4, %r9
-	loop loop_vec_diff
-
-end_vec_diff:
-	movl num(%rip), %eax
-	subl %edi, %eax
-
-	ret
-
-# -----------------------------------------------------
-
 exists:
-
-	movl x(%rip), %esi
-	movl $0, %eax # set eax (result) to 0
-	movq num(%rip), %rcx # mov vec lenght to rcx
-	movq ptrvec(%rip), %r8 # move vec pointer to r8
+	movl $0, %eax
+	movl x(%rip), %edi
+	movslq num(%rip), %rcx
+	movq ptrvec(%rip), %rdx
 
 loop_exists:
-	cmpl (%r8), %esi # check if poited value is equal to x
-	je found
-	
-	addq $4, %r8
-	loop loop_exists
+	cmpl (%rdx), %edi
+	jne not_equal
 
-found:
 	incl %eax
 	cmpl $2, %eax
 	je ret_true
-	
-	addq $4, %r8
+
+not_equal:
+	addq $4, %rdx
 	loop loop_exists
 	
+	movl $0, %eax
+	ret
+
 ret_true:
-	movl $1, %eax # return 1 (is duplicated)
+	movl $1, %eax
 	ret
 
-ret_false:
-	movl $0, %eax # return 0 (is not duplicated)
-	ret
+# ------------------------------------
 
+vec_diff:
+
+	movslq num(%rip), %rcx
+	movq ptrvec(%rip), %r8
+	movl $0, %esi
+
+
+	cmpq $0, %rcx
+	je end
+
+
+loop:
+	movl (%r8), %r9d
+	movl %r9d, x(%rip)
+
+	movq %rcx, %r9
+
+	call exists
 	
+	movq %r9, %rcx
+
+	addl %eax, %esi
+
+	addq $4, %r8
+
+	loop loop
+	
+end:
+	movl num(%rip), %eax
+	subl %esi, %eax
+	ret
