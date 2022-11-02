bd3a7383340d02c6a48cc2d30ede4b7d782448c9
André Barros <andrebarros0426@gmail.com>
2022-11-02 17:53:35 +0000

    fix: makefile

--- modulo3/ex17/main.c
+++ modulo3/ex17/main.c
@@ -2,12 +2,12 @@
 #include "asm.h"
 
 int vec[] = {1,2,3,4,5};
-int* ptrvec = vec;
+int* ptrvec = &vec[0];
 int num = 5;
 
 int main(void) {
 
-    exists();
+    array_sort();
 
     for(int i = 0; i<5; i++){
         printf("%d", vec[i]);
