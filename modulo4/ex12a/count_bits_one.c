255eb0bba719f4d83fa940a2a89ef87094c60744
carlos <1211277@isep.ipp.pt>
2022-11-16 17:48:59 +0000

    feat: ex12a mod4

--- modulo4/ex12a/count_bits_one.c
+++ modulo4/ex12a/count_bits_one.c
@@ -2,7 +2,10 @@
 
 int count_bits_one(int x) {
     int count = 0;
-    for(int i = 0; i<31; i++){
-        cont += x 
-    }    
+    for(int i = 0; i<32; i++){
+        count += (x & 1);
+        x >>= 1; 
+    }  
+    return count;  
 }
+
