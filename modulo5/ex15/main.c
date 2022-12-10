4947afb9cd53472822c5a5d9d6d83258b0fc7fc1
André Barros <andrebarros0426@gmail.com>
2022-12-10 16:01:19 +0000

    feat: ex15 mod5

---
+++ modulo5/ex15/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "stack.h"
+#include <stdlib.h>
+
+int main(void) {
+
+  stack s;
+  s.size = 0;
+  s.stack = (long*) malloc(s.size*sizeof(long));
+  
+  
+  printf("\ninitial stack size = %d\n\n", s.size);
+  
+  printf("push of number 100\n");
+  push(&s,100);
+  printf("stack size after 1 push = %d\n\n", s.size);
+  
+  printf("push of number 150\n");
+  push(&s,150);
+  printf("stack size after 2 push = %d\n\n", s.size);
+  
+  long value = pop(&s);
+  printf("stack size after 1 pop = %d\n", s.size);
+  printf("value pop = %ld\n\n", value);
+
+  long value2 = pop(&s);
+  printf("stack size after 2 pop = %d\n", s.size);
+  printf("value pop = %ld\n\n", value2);
+  
+  return 0; 
+}
