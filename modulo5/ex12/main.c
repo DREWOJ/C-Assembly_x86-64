6ed4fef073b98a003eebad912869474d3bab9b71
carlos <1211277@isep.ipp.pt>
2022-12-11 01:09:28 +0000

    fix: ex12 main

--- modulo5/ex12/main.c
+++ modulo5/ex12/main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function.h"
 
 int main(void) {
 
-  int **matrix = new_matrix(5,5);
+  int **matrix = new_matrix(2,2);
 
-  matrix[0] = {4,2,5,1,7};
-  matrix[1] = {5,2,6,1,7};
-  matrix[2] = {8,6,4,4,8};
-  matrix[3] = {6,3,1,7,3};
-  matrix[4] = {1,4,6,2,7};
+  matrix[0][0] = 2;
+  matrix[0][1] = 7;
+  matrix[1][0] = 1;
+  matrix[1][1] = 3;
 
-  int result = find_matrix
+  printf("%d\n",find_matrix(matrix, 2, 2, 1));
+
+  free(matrix);
 
   return 0;
 }
